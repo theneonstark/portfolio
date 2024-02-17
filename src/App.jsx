88cf5fdefcb647edc97212a46e379eb42cd206d8
Stark <mohd47149@gmail.com>
2024-02-18 01:13:24 +0530

    BHEN KA LODA EXTN. HI NHI MILA MAI NHI BNA RAHA MKC ISKI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./app.scss"
+import "./app.scss";
+import Nav from "./components/navbar/nav";
 
 const App = () => {
     return <div>
         <section>
-            <a href="#">Home</a>
+            <Nav/>
         </section>
         <section>Helo</section>
         <section>Helo</section>
---
+++ src/components/navbar/nav.jsx
@@ -0,0 +1,15 @@
+import "./nav.scss";
+
+const nav = () => {
+  return (
+    <div className="nav">
+      {/* {Sidebar} */}
+      <div className="header">
+        <span>StarK</span>
+      </div>
+    </div>
+  )
+}
+
+export default nav
+
