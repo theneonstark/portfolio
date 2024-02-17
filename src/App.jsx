40ba07c98d13524e157bb969ed2c02e0e55ebec0
Shahid <shahid@artheducation.com>
2024-02-17 17:13:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./app.scss"
+
+const App = () => {
+    return <div>
+        <section>
+            <a href="#">Home</a>
+        </section>
+        <section>Helo</section>
+        <section>Helo</section>
+        <section>Helo</section>
+        <section>Helo</section>
+        <section>Helo</section>
+        <section>Helo</section>
+    </div>;
+}
+
+export default App;
