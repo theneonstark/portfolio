aa49c21fe168adec0b08a51c99069bf74ea5cd96
Stark <mohd47149@gmail.com>
2024-02-19 00:51:03 +0530

    make header and extension work

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,4 +1,6 @@
 import "./nav.scss";
+import 'boxicons';
+
 
 const nav = () => {
   return (
@@ -6,6 +8,12 @@ const nav = () => {
       {/* {Sidebar} */}
       <div className="header">
         <span>StarK</span>
+        <div className="links">
+          <a href="#"><box-icon type='logo' color="#316ff6" name='facebook-square'></box-icon></a>
+          <a href="#"><box-icon type='logo' color="#0077b5" name='linkedin-square'></box-icon></a>
+          <a href="#"><box-icon type='logo' color="#d62976" name='instagram-alt'></box-icon></a>
+          <a href="#"><box-icon type='logo' name='github'></box-icon></a>
+        </div>
       </div>
     </div>
   )
